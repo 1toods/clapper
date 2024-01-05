2caf9c5dca95a3eb8240ab63bf51338285002af5
Stefan Baumann <stefan.baumann@student.tugraz.at>
2024-01-05 11:42:32 +0100

    start with pipeline

--- src/main.py
+++ src/main.py
@@ -2,10 +2,40 @@
 
 import os
 import time
+import argparse
 
 
 def main():
-    print("hello world")
+    parser = argparse.ArgumentParser(
+        prog='SWEB Tester',
+        description='Testrunner for SWEB'
+    )
+    
+    parser.add_argument(
+        '-l',
+        '--list-tests',
+        action='store_true',
+        help='Shows a list of available tests to run.'
+    )
+    
+    # this should be the default setting
+    parser.add_argument(
+        '-a',
+        '--run-all',
+        action='store_true',
+        help='Runs all available tests in sequence.'
+    )
+
+    parser.add_argument(
+        '-r',
+        '--run-test',
+        nargs='?',
+        help='Specify test to run. If this argument is given, only the specifyed tests will run.'
+    )
+
+    arguments = parser.parse_args()
+
+    print(arguments)
 
 if __name__ == '__main__':
     main()
